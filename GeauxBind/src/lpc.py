ca35016fe8e4ef64fd48497b8966b792f10f147f
jaydy <jaydy@qb.loni.org>
2016-02-05 19:21:50 -0600

    execute lpc within python script
    	modified:   GeauxBind/src/lpc.py

--- GeauxBind/src/lpc.py
+++ GeauxBind/src/lpc.py
@@ -2,6 +2,10 @@
 
 import os
 import pybel
+import tempfile
+import shutil
+import subprocess32
+import shlex
 
 
 class LPC:
@@ -25,16 +29,33 @@ class LPC:
         to_write.append("END\n")
         return "".join(to_write)
 
-    def runLPC(self):
+    def runLPC(self, lpc_bin="/home/jaydy/local/LPC/lpcEx"):
         prt = pybel.readfile(self._prt_format, self._prt_path).next()
         lig = pybel.readfile(self._lig_format, self._lig_path).next()
-        self.merge(prt, lig)
+        merged = self.merge(prt, lig)
+
+        try:
+            tmp_dir = tempfile.mkdtemp()
+            merged_pdb_path = os.path.join(tmp_dir, 'merged.pdb')
+            with open(merged_pdb_path, 'w') as ofs:
+                ofs.write(merged)
+            cmds = "{} 1 {}".format(lpc_bin, merged_pdb_path)
+            print(cmds)
+            cmds = shlex.split(cmds)
+            os.chdir(tmp_dir)
+            subprocess32.call(cmds)
+            with open(os.path.join(tmp_dir, 'RES1')) as ifs:
+                result = ifs.read()
+            return result
+        finally:
+            shutil.rmtree(tmp_dir)
 
 
 def test():
     lpc = LPC("/work/jaydy/dat/edud/crystal-ligands-sdf/1b9v.sdf",
               "/work/jaydy/dat/edud/crystal-structure/1b9vA.pdb")
-    lpc.runLPC()
+    lpc_result = lpc.runLPC()
+    print(lpc_result)
 
 
 if __name__ == '__main__':
